34a3543ae863ef5b13eab2d0abbdaf81bba8cdd8
Wishva Kalhara <vishvakalhara@gmail.com>
2024-09-06 16:09:11 +0530

    Issue Subscription

--- src/views/dialogs/DlgSubscription.java
+++ src/views/dialogs/DlgSubscription.java
@@ -7,6 +7,7 @@ package views.dialogs;
 import com.formdev.flatlaf.FlatClientProperties;
 import com.wishva.*;
 import controllers.SubscriptionController;
+import enums.DialogTypes;
 import java.awt.Color;
 import java.util.Date;
 import java.util.Vector;
@@ -27,7 +28,7 @@ import views.layouts.AppLayout;
  * @author vishv
  */
 public class DlgSubscription extends javax.swing.JDialog {
-
+    
     HashMap<String, String> customerMap = new HashMap();
     HashMap<String, PaymentPlan> packagesMap = new HashMap();
 
@@ -37,28 +38,27 @@ public class DlgSubscription extends javax.swing.JDialog {
     public DlgSubscription(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
-
+        
         setDesign();
-
-        cboCustomerName.grabFocus();
+        
+        txtCustomerId.grabFocus();
         dateFrom.setDate(new Date());
         dateFrom.setMinSelectableDate(new Date());
-
+        
         fetchData();
     }
-
+    
     private void setDesign() {
-
+        
         getRootPane().putClientProperty(FlatClientProperties.TITLE_BAR_BACKGROUND, new Color(255, 255, 255));
         getRootPane().putClientProperty(FlatClientProperties.TITLE_BAR_FOREGROUND, new Color(0, 0, 0));
         getRootPane().putClientProperty(FlatClientProperties.TITLE_BAR_SHOW_CLOSE, false);
         getRootPane().putClientProperty(FlatClientProperties.TITLE_BAR_SHOW_MAXIMIZE, false);
         getRootPane().putClientProperty(FlatClientProperties.TITLE_BAR_SHOW_ICONIFFY, false);
-
+        
         btnSubmit.putClientProperty("JButton.buttonType", "borderless");
         btnClose.putClientProperty("JButton.buttonType", "borderless");
-        btnScan.putClientProperty("JButton.buttonType", "borderless");
-
+        
         txtCustomerId.putClientProperty(FlatClientProperties.STYLE, "arc: 10");
         txtCustomerId.putClientProperty(FlatClientProperties.PLACEHOLDER_TEXT, "cus-");
     }
@@ -84,7 +84,6 @@ public class DlgSubscription extends javax.swing.JDialog {
         cboPackage = new javax.swing.JComboBox<>();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
-        btnScan = new javax.swing.JButton();
         dateFrom = new com.toedter.calendar.JDateChooser();
         txtCustomerId = new javax.swing.JTextField();
         jPanel2 = new javax.swing.JPanel();
@@ -107,7 +106,7 @@ public class DlgSubscription extends javax.swing.JDialog {
         });
 
         lblHeading.setFont(new java.awt.Font("Segoe UI Semibold", 0, 24)); // NOI18N
-        lblHeading.setText("New Subscription");
+        lblHeading.setText("Issue a subscription");
 
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
         jPanel3.setLayout(jPanel3Layout);
@@ -166,10 +165,6 @@ public class DlgSubscription extends javax.swing.JDialog {
         jLabel5.setForeground(new java.awt.Color(102, 102, 102));
         jLabel5.setText("Pacakge:");
 
-        btnScan.setBackground(new java.awt.Color(249, 249, 249));
-        btnScan.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/icons/scan-barcode.png"))); // NOI18N
-        btnScan.setMargin(new java.awt.Insets(0, 14, 0, 14));
-
         txtCustomerId.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 txtCustomerIdActionPerformed(evt);
@@ -186,8 +181,7 @@ public class DlgSubscription extends javax.swing.JDialog {
                     .addGroup(jPanel5Layout.createSequentialGroup()
                         .addComponent(cboPackage, javax.swing.GroupLayout.PREFERRED_SIZE, 309, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addComponent(dateFrom, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 0, Short.MAX_VALUE))
+                        .addComponent(dateFrom, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel5Layout.createSequentialGroup()
                         .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel2)
@@ -197,18 +191,13 @@ public class DlgSubscription extends javax.swing.JDialog {
                             .addGroup(jPanel5Layout.createSequentialGroup()
                                 .addGap(18, 18, 18)
                                 .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addGroup(jPanel5Layout.createSequentialGroup()
-                                        .addComponent(jLabel3)
-                                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel5Layout.createSequentialGroup()
-                                        .addComponent(cboCustomerName, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                        .addComponent(btnScan, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addContainerGap(32, Short.MAX_VALUE))))
+                                    .addComponent(jLabel3)
+                                    .addComponent(cboCustomerName, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                             .addGroup(jPanel5Layout.createSequentialGroup()
                                 .addGap(185, 185, 185)
                                 .addComponent(jLabel4)
-                                .addGap(0, 0, Short.MAX_VALUE))))))
+                                .addGap(0, 0, Short.MAX_VALUE)))))
+                .addGap(32, 32, 32))
         );
         jPanel5Layout.setVerticalGroup(
             jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -219,9 +208,7 @@ public class DlgSubscription extends javax.swing.JDialog {
                     .addComponent(jLabel3))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(cboCustomerName, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(btnScan, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(cboCustomerName, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(txtCustomerId))
                 .addGap(25, 25, 25)
                 .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -288,32 +275,32 @@ public class DlgSubscription extends javax.swing.JDialog {
     }//GEN-LAST:event_btnCloseActionPerformed
 
     private void cboCustomerNameActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cboCustomerNameActionPerformed
-
+        
         if (cboCustomerName.getSelectedIndex() == 0) {
             txtCustomerId.setText("");
             return;
         }
-
+        
         txtCustomerId.setText(customerMap.get(String.valueOf(cboCustomerName.getSelectedItem())));
     }//GEN-LAST:event_cboCustomerNameActionPerformed
 
     private void txtCustomerIdActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtCustomerIdActionPerformed
-
+        
         String input = txtCustomerId.getText();
         if (input.isBlank()) {
             return;
         }
-
+        
         try {
-
+            
             if (!input.startsWith("cus-") && Character.isDigit(input.charAt(0))) {
                 txtCustomerId.setText("cus-" + input);
             }
-
+            
             ResultSet rs = AppConnection.fetch("SELECT CONCAT(`first_name`, ' ', `last_name`) AS full_name "
                     + "FROM customers "
                     + "WHERE `id` = '" + txtCustomerId.getText() + "' AND `statuses_id` = '1'");
-
+            
             if (rs.next()) {
                 cboCustomerName.setSelectedItem(rs.getString("full_name"));
                 cboPackage.grabFocus();
@@ -327,36 +314,39 @@ public class DlgSubscription extends javax.swing.JDialog {
     }//GEN-LAST:event_txtCustomerIdActionPerformed
 
     private void cboPackageActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cboPackageActionPerformed
-
+        
         if (cboPackage.getSelectedIndex() == 0) {
             btnSubmit.setText("Select a Package");
             return;
         }
-
+        
         String price = new Formatter().addZeroToDouble(packagesMap.get(String.valueOf(cboPackage.getSelectedItem())).getPrice());
         btnSubmit.setText("Issue Subscription (" + AppLayout.appData.getCurrencyValue() + " " + price + ")");
     }//GEN-LAST:event_cboPackageActionPerformed
 
     private void btnSubmitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSubmitActionPerformed
-
+        
         try {
-
+            
             Subscription subscription = getSubscriptionFromForm();
             subscription.setPaidAmount(packagesMap.get(String.valueOf(cboPackage.getSelectedItem())).getPrice());
             
-            new SubscriptionController().createSubscription(subscription);
+            new DlgPayment(AppLayout.appLayout, true, subscription).setVisible(true);
+            this.dispose();
+            
+            new DlgError(AppLayout.appLayout, true, "Subscription Issued Success.", "Success", DialogTypes.SUCCESS).setVisible(true);
+            
         } catch (SparkException e) {
             new DlgError(AppLayout.appLayout, true, "Validation Error", e.getMessage()).setVisible(true);
         } catch (Exception e) {
             e.printStackTrace();
         }
-
+        
 
     }//GEN-LAST:event_btnSubmitActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnClose;
-    private javax.swing.JButton btnScan;
     private javax.swing.JButton btnSubmit;
     private javax.swing.JComboBox<String> cboCustomerName;
     private javax.swing.JComboBox<String> cboPackage;
@@ -378,98 +368,96 @@ public class DlgSubscription extends javax.swing.JDialog {
         new Thread(new Runnable() {
             @Override
             public void run() {
-
+                
                 loadCustomers();
                 loadPackages();
             }
         }).start();
     }
-
+    
     private void loadCustomers() {
-
+        
         try {
-
+            
             Vector<String> customers = new Vector();
             customers.add("Select");
             customerMap.put("Select", "0");
-
+            
             ResultSet rs = AppConnection.fetch("SELECT `id`, CONCAT(`first_name`, ' ', `last_name`) AS full_name "
                     + "FROM customers "
                     + "WHERE `statuses_id` = '1' "
                     + "ORDER BY `first_name` ASC");
-
+            
             while (rs.next()) {
                 String fullName = rs.getString("full_name");
                 customers.add(fullName);
                 customerMap.put(fullName, rs.getString("id"));
             }
-
+            
             cboCustomerName.setModel(new DefaultComboBoxModel<String>(customers));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
+    
     private void loadPackages() {
         try {
-
+            
             Vector<String> packages = new Vector();
             packages.add("Select");
-
+            
             packagesMap.put("Select", new PaymentPlan());
-
+            
             ResultSet rs = AppConnection.fetch("SELECT `id`, CONCAT(`title`, ' - ', `validity`, 'd') as displayName, `price`, `validity` "
                     + "FROM members_v2.packages "
                     + "WHERE `statuses_id` = '1';");
-
+            
             while (rs.next()) {
                 String fullName = rs.getString("displayName") + " (" + AppLayout.appData.getCurrencyValue() + rs.getString("price") + ")";
                 packages.add(fullName);
-
+                
                 PaymentPlan plan = new PaymentPlan();
                 plan.setPrice(rs.getDouble("price"));
                 plan.setId(rs.getString("id"));
                 plan.setValidity(rs.getInt("validity"));
                 packagesMap.put(fullName, plan);
             }
-
+            
             cboPackage.setModel(new DefaultComboBoxModel<>(packages));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
+    
     private Subscription getSubscriptionFromForm() throws SparkException {
-
+        
         Subscription subscription = new Subscription();
-
+        
         subscription.setCustomerId(new Spark("Customer Id", txtCustomerId.getText())
                 .required()
                 .endString());
         
-        if(cboCustomerName.getSelectedIndex() == 0)
+        if (cboCustomerName.getSelectedIndex() == 0) {
             throw new SparkException("Select a customer name!");
+        }
         
-        if(!customerMap.get(String.valueOf(cboCustomerName.getSelectedItem())).equals(subscription.getCustomerId())){
+        if (!customerMap.get(String.valueOf(cboCustomerName.getSelectedItem())).equals(subscription.getCustomerId())) {
             txtCustomerId.setText("");
             cboCustomerName.setSelectedIndex(0);
             throw new SparkException("Select the customer again!");
         }
         
-        if(cboPackage.getSelectedIndex() == 0)
+        if (cboPackage.getSelectedIndex() == 0) {
             throw new SparkException("Select a package!");
+        }
         subscription.setPackageId(packagesMap.get(String.valueOf(cboPackage.getSelectedItem())).getId());
         
-        if(dateFrom.getDate() == null)
+        if (dateFrom.getDate() == null) {
             throw new SparkException("Select a start date!");
+        }
         subscription.setStartDate(dateFrom.getDate());
         subscription.setValidity(packagesMap.get(String.valueOf(cboPackage.getSelectedItem())).getValidity());
         
         return subscription;
     }
 }
-
-
-
-
-
